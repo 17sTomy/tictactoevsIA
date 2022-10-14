1de5e767862df4f07d555c56d3f3ea2a422d41be
17sTomy <tomasignacioalv@hotmail.com>
2022-10-13 21:47:20 -0300

    making it invincible

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const GAME_BOARD = document.querySelector(".game-container"),
     NOTIFICATION = document.querySelector(".game-notification"),
     GAME_STATUS = ["", "", "", "", "", "", "", "", ""],
     WINNINGS = [
+        [0, 4, 8],
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
         [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
-        [0, 4, 8],
         [2, 4, 6]
     ]
 
